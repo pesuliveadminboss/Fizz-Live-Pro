d2437da15a479f84a28ddb0e7e00efcd8bf6674f
pesuliveadminboss <pesulive8@gmail.com>
2026-09-20 13:56:38 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
 import '2_auth/auth_screens.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  // Global bindings & async initializations if needed
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: AgeGateAndAuthScreen(),
-  ));
-}
-
+void main() => runApp(const MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: SplashLoginScreen(),
+));
