ae4ad58c2972cfe911fdb38eaf9ee768477cd2f3
pesuliveadminboss <pesulive8@gmail.com>
2026-09-21 00:44:08 +0530

    Update call_and_party_screens.dart

--- lib/4_interactions/call_and_party_screens.dart
+++ lib/4_interactions/call_and_party_screens.dart
@@ -118,7 +118,7 @@ class _PartyRoomGridWidgetState extends State<PartyRoomGridWidget> {
   void initState() {
     super.initState();
     seats[0] = 'Host_Alpha';
-    seats = 'Guest_01'; // Fixed list index assignment
+    seats = 'Guest_01';
   }
 
   void _tapSeat(int index) {
