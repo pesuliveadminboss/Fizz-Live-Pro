319b10c4d372e80b5398d7d23b4b15b19157815f
pesuliveadminboss <pesulive8@gmail.com>
2026-09-22 10:06:13 +0530

    Update gift_sheet.dart

--- lib/4_interactions/gift_sheet.dart
+++ lib/4_interactions/gift_sheet.dart
@@ -78,7 +78,7 @@ void showRechargeModal(BuildContext context) {
 void showGiftSendingSheet(BuildContext context, String targetName) {
   String selectedCategory = 'Bag';
   int selectedQuantity = 1;
-  const List<int> quantOptions =;
+  const List<int> quantOptions =[ 1,7,77,177,777 ];
   
   
 
