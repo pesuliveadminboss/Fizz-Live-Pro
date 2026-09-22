488ee04ba1151352de59bfc90c6266924c52fb58
pesuliveadminboss <pesulive8@gmail.com>
2026-09-22 21:02:36 +0530

    Create constants.dart

---
+++ lib/core/constants.dart
@@ -0,0 +1,7 @@
+class AppConstants {
+  static const String appName = 'Fizz Live Pro';
+  // Zego AppID and AppSign (unoda zego credentialsanga podu)
+  static const int zegoAppId = 123456789; 
+  static const String zegoAppSign = "your_zego_app_sign_here";
+}
+
