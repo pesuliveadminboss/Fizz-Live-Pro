115f0bccc54809bc7e3de1218b5f91c9d348ae80
pesuliveadminboss <pesulive8@gmail.com>
2026-09-22 23:42:30 +0530

    Delete lib directory

--- lib/core/constants.dart
+++
@@ -1,7 +0,0 @@
-class AppConstants {
-  static const String appName = 'Fizz Live Pro';
-  // Zego AppID and AppSign (unoda zego credentialsanga podu)
-  static const int zegoAppId = 123456789; 
-  static const String zegoAppSign = "your_zego_app_sign_here";
-}
-
