3509b96555a28ab5fc25c5f74f4f4d98e4c9d127
pesuliveadminboss <pesulive8@gmail.com>
2026-09-23 16:09:20 +0530

    Update app_state.dart

--- lib/providers/app_state.dart
+++ lib/providers/app_state.dart
@@ -2,13 +2,24 @@ import 'package:flutter/material.dart';
 
 class AppState extends ChangeNotifier {
   int _gems = 450;
+  String _userName = 'Fizz User';
+  String _userHandle = '@fizzuser_101';
+  
   final List<Map<String, dynamic>> _transactions = [
     {'title': 'Recharge 500 Gems', 'date': 'Today, 2:15 PM', 'amount': '+500 Gems', 'isCredit': true},
     {'title': 'Gift sent to Anitha_Live', 'date': 'Yesterday, 10:40 PM', 'amount': '-50 Gems', 'isCredit': false},
   ];
 
   int get gems => _gems;
+  String get userName => _userName;
+  String get userHandle => _userHandle;
   List<Map<String, dynamic>> get transactions => _transactions;
+
+  void updateProfile(String name, String handle) {
+    _userName = name;
+    _userHandle = handle.startsWith('@') ? handle : '@$handle';
+    notifyListeners();
+  }
 
   bool sendGift(String receiver, String giftName, int cost) {
     if (_gems >= cost) {
