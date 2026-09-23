eb64e09b7ef8f9f3fadbdaecfef542636fe06a17
pesuliveadminboss <pesulive8@gmail.com>
2026-09-23 13:44:25 +0530

    Delete lib directory

--- lib/fizz_core.dart
+++
@@ -1,8 +0,0 @@
-import 'package:get/get.dart';
-
-enum ConnectionStateEnum { connected, disconnected, connecting }
-
-class FizzCoreController extends GetxController {
-  var connectionState = ConnectionStateEnum.connected.obs;
-}
-
