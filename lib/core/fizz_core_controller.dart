bae5a90ffed88d7a01eefbe8ace49a0f81c39b93
pesuliveadminboss <pesulive8@gmail.com>
2026-09-23 09:01:16 +0530

    Create fizz_core_controller.dart

---
+++ lib/core/fizz_core_controller.dart
@@ -0,0 +1,7 @@
+enum ConnectionStateEnum { connected, disconnected, connecting }
+
+class FizzCoreController {
+  var connectionState = ConnectionStateEnum.connected.obs;
+}
+*(Note: GetX use panrathala import 'package:get/get.dart'; podanum)*
+  
