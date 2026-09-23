779c5b0abf64400098304a73dea21c99acb41db2
pesuliveadminboss <pesulive8@gmail.com>
2026-09-23 10:08:45 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:fizz_live_pro/fizz_core.dart';
-import 'package:fizz_live_pro/views/login_view.dart';
+import 'core/user_prefs.dart';
+import 'views/intro_view.dart';
+import 'views/login_view.dart';
+import 'views/profile_setup_view.dart';
+import 'views/home_feed_view.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Get.put(FizzCoreController(), permanent: true);
-  runApp(const FizzLiveApp());
+  bool loggedIn = await UserPrefs.isLoggedIn();
+  bool profileDone = await UserPrefs.isProfileDone();
+
+  Widget initialScreen = const IntroView();
+  if (loggedIn && profileDone) {
+    initialScreen = const HomeFeedView();
+  } else if (loggedIn && !profileDone) {
+    initialScreen = const ProfileSetupView();
+  }
+
+  runApp(FizzLiveApp(initialScreen: initialScreen));
 }
 
 class FizzLiveApp extends StatelessWidget {
-  const FizzLiveApp({super.key});
+  final Widget initialScreen;
+  const FizzLiveApp({super.key, required this.initialScreen});
 
   @override
   Widget build(BuildContext context) {
@@ -18,8 +31,7 @@ class FizzLiveApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Fizz Live Pro',
       theme: ThemeData.dark(),
-      home: const LoginView(),
+      home: initialScreen,
     );
   }
 }
-
