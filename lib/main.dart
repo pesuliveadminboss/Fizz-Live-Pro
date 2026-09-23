66ea3c858063d687a9e81e45e9454e3c5502b496
pesuliveadminboss <pesulive8@gmail.com>
2026-09-23 09:06:16 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'controllers/fizz_core_controller.dart';
-import 'views/splash_view.dart';
+import 'package:fizz_live_pro/core/fizz_core_controller.dart';
+import 'package:fizz_live_pro/views/login_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   Get.put(FizzCoreController(), permanent: true);
-  runApp(GetMaterialApp(
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData.dark(),
-    home: SplashView(),
-  ));
+  runApp(const FizzLiveApp());
+}
+
+class FizzLiveApp extends StatelessWidget {
+  const FizzLiveApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Fizz Live Pro',
+      theme: ThemeData.dark(),
+      home: const LoginView(),
+    );
+  }
 }
