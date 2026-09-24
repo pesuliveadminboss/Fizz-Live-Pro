3990b02048aa754776bcc8694d6313ae48cce234
pesuliveadminboss <pesulive8@gmail.com>
2026-09-24 11:59:05 +0530

    Update auth_and_popups.dart

--- lib/auth_and_popups.dart
+++ lib/auth_and_popups.dart
@@ -1,7 +1,7 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'main.dart';
+import 'core_data.dart';
 
 class EntryPopupsHelper {
   static void showDailyRewardsDialog(BuildContext context) {
@@ -384,5 +384,4 @@ class _LoginScreenState extends State<LoginScreen> {
 
   void _showGoogleAccountPicker(BuildContext context) {
     showModalBottomSheet(
-      context: context,
- 
+      context: conte
