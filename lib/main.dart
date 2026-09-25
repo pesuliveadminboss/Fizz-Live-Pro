b0bcf08a0ec5e67c175e06e68103b8c61597b539
pesuliveadminboss <pesulive8@gmail.com>
2026-09-25 12:50:14 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,7 @@ import 'block1_auth_onboarding/authorization_settings_dialog.dart';
 import 'block1_auth_onboarding/call_reminder_dialog.dart';
 import 'block2_navigation_shell/main_shell_navigation.dart';
 
-void main() {
-  runApp(const FizzLiveProApp());
-}
+void main() => runApp(const FizzLiveProApp());
 
 class FizzLiveProApp extends StatelessWidget {
   const FizzLiveProApp({super.key});
@@ -18,56 +16,38 @@ class FizzLiveProApp extends StatelessWidget {
     return MaterialApp(
       title: 'Fizz Live Pro',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        scaffoldBackgroundColor: const Color(0xFF0F0F1A),
-        primaryColor: const Color(0xFFE94057),
-      ),
+      theme: ThemeData(brightness: Brightness.dark, scaffoldBackgroundColor: const Color(0xFF0F0F1A)),
       initialRoute: '/',
       routes: {
         '/': (context) => const LoginAuthScreen(),
         '/profile_setup': (context) => const ProfileSetupScreen(),
-        '/popups': (context) => const PopupsHomeWrapper(),
+        '/popups': (context) => const PopupsWrapper(),
         '/main_shell': (context) => const MainShellNavigation(),
       },
     );
   }
 }
 
-// Temporary wrapper to show entry popups right after profile setup
-class PopupsHomeWrapper extends StatefulWidget {
-  const PopupsHomeWrapper({super.key});
+class PopupsWrapper extends StatefulWidget {
+  const PopupsWrapper({super.key});
 
   @override
-  State<PopupsHomeWrapper> createState() => _PopupsHomeWrapperState();
+  State<PopupsWrapper> createState() => _PopupsWrapperState();
 }
 
-class _PopupsHomeWrapperState extends State<PopupsHomeWrapper> {
+class _PopupsWrapperState extends State<PopupsWrapper> {
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      // Show Daily Rewards, Authorization Settings, and Call Reminder dialogs sequentially
-      showDialog(
-        context: context,
-        builder: (_) => const DailyRewardsDialog(),
-      ).then((_) {
-        showDialog(
-          context: context,
-          builder: (_) => const AuthorizationSettingsDialog(),
-        ).then((_) {
-          showDialog(
-            context: context,
-            builder: (_) => const CallReminderDialog(),
-          );
+      showDialog(context: context, builder: (_) => const DailyRewardsDialog()).then((_) {
+        showDialog(context: context, builder: (_) => const AuthorizationSettingsDialog()).then((_) {
+          showDialog(context: context, builder: (_) => const CallReminderDialog());
         });
       });
     });
   }
 
   @override
-  Widget build(BuildContext context) {
-    return const MainShellNavigation();
-  }
+  Widget build(BuildContext context) => const MainShellNavigation();
 }
-
