58af76cae354491fbacb0d915bbffd415dd4152b
pesuliveadminboss <pesulive8@gmail.com>
2026-09-25 15:06:38 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,9 @@ import 'block1_auth_onboarding/authorization_settings_dialog.dart';
 import 'block1_auth_onboarding/call_reminder_dialog.dart';
 import 'block2_navigation_shell/main_shell_navigation.dart';
 
-void main() => runApp(const FizzLiveProApp());
+void main() {
+  runApp(const FizzLiveProApp());
+}
 
 class FizzLiveProApp extends StatelessWidget {
   const FizzLiveProApp({super.key});
@@ -16,38 +18,52 @@ class FizzLiveProApp extends StatelessWidget {
     return MaterialApp(
       title: 'Fizz Live Pro',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(brightness: Brightness.dark, scaffoldBackgroundColor: const Color(0xFF0F0F1A)),
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: const Color(0xFF0F0F1A),
+        primaryColor: const Color(0xFFE94057),
+      ),
       initialRoute: '/',
       routes: {
         '/': (context) => const LoginAuthScreen(),
-        '/profile_setup': (context) => const ProfileSetupScreen(),
-        '/popups': (context) => const PopupsWrapper(),
+        '/popups': (context) => const PopupsHomeWrapper(),
         '/main_shell': (context) => const MainShellNavigation(),
       },
     );
   }
 }
 
-class PopupsWrapper extends StatefulWidget {
-  const PopupsWrapper({super.key});
+class PopupsHomeWrapper extends StatefulWidget {
+  const PopupsHomeWrapper({super.key});
 
   @override
-  State<PopupsWrapper> createState() => _PopupsWrapperState();
+  State<PopupsHomeWrapper> createState() => _PopupsHomeWrapperState();
 }
 
-class _PopupsWrapperState extends State<PopupsWrapper> {
+class _PopupsHomeWrapperState extends State<PopupsHomeWrapper> {
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      showDialog(context: context, builder: (_) => const DailyRewardsDialog()).then((_) {
-        showDialog(context: context, builder: (_) => const AuthorizationSettingsDialog()).then((_) {
-          showDialog(context: context, builder: (_) => const CallReminderDialog());
+      showDialog(
+        context: context,
+        builder: (_) => const DailyRewardsDialog(),
+      ).then((_) {
+        showDialog(
+          context: context,
+          builder: (_) => const AuthorizationSettingsDialog(),
+        ).then((_) {
+          showDialog(
+            context: context,
+            builder: (_) => const CallReminderDialog(),
+          );
         });
       });
     });
   }
 
   @override
-  Widget build(BuildContext context) => const MainShellNavigation();
+  Widget build(BuildContext context) {
+    return const MainShellNavigation();
+  }
 }
